42ce675ebf84e22babb8fa3324aeccfee03d7a0d
Yu Chen Hou <houyuchen66@gmail.com>
2016-06-12 18:17:34 -0400

    main

--- Straights.cpp
+++ Straights.cpp
@@ -1,4 +1,15 @@
+#include "GameController.h"
+#include <cstdlib>
+#include <memory>
+
+using namespace std;
+
 int main(int argc, char *argv[]) {
+	int seed = 0;
+	if (argc > 0) {
+		seed = atoi(argv[0]);
+	}
+	shared_ptr<GameController> controller = GameController::createInstance(seed);
 
 	return 0;
 }
