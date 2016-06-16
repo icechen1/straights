ff6c951dcd34e3c513f1c088157412d9aeda9d24
Yu Chen Hou <houyuchen66@gmail.com>
2016-06-16 02:10:18 -0400

    more bs

--- Straights.cpp
+++ Straights.cpp
@@ -8,7 +8,7 @@ std::shared_ptr<GameController> GameController::instance_; // WHYYY?
 
 int main(int argc, char *argv[]) {
 	int seed = 0;
-	if (argc > 0) {
+	if (argc > 1) {
 		seed = atoi(argv[1]);
 	}
 	shared_ptr<GameController> controller = GameController::createInstance(seed);
