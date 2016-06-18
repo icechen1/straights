9d551d3b66e10637fbfb89c1bb68c0d213a03b20
Yu Chen Hou <houyuchen66@gmail.com>
2016-06-18 01:47:52 -0400

    comments to round

--- Round.cpp
+++ Round.cpp
@@ -3,11 +3,16 @@
 
 using namespace std;
 
+// ensures: set initial random dealt cards and starting player
+// returns: a constructed Round object
 Round::Round(shared_ptr<GameState> _state) : gameState_(_state) {
 	dealCards();
 	findStartingPlayer();
 }
 
+// requires: gameState_ is not empty, and has players
+// modifies: sets the currentPlayer_ to the player with a 7S
+// ensures: does not modify the players
 void Round::findStartingPlayer() {
 	for (shared_ptr<Player> p : gameState_->players_) {
 		for (Card c : p->getHand()) {
@@ -20,6 +25,10 @@ void Round::findStartingPlayer() {
 	}
 }
 
+// requires: gameState_ is not empty, and has players
+// requires: there are cards in the deck
+// modifies: deals 13 random cards to each player (class field)
+// ensures: does not modify the game state
 void Round::dealCards() {
 	gameState_->deck_->shuffle();
 	deque<shared_ptr<Card>> cards = gameState_->deck_->getCards();
@@ -31,25 +40,37 @@ void Round::dealCards() {
 	}
 }
 
+// requires: valid player and command
+// modifies: list of legal moves
+// ensures: player.play() gets called
 void Round::handleTurn() {
 	shared_ptr<Player> p = currentPlayer_;
+	// compute the list of legal moves
 	p->computeLegalMoves(firstTurn_);
-
+	// ask the player to make a move
 	Command c = p->play();
+	// play move
 	playTurn(p, c);
 }
 
+// requires: valid player
+// modifies: state about current round
+// ensures: a full round is played after the function runs
 void Round::playRound() {
+	// print round start message
 	GameView::startRound(currentPlayer_->getPlayerId() + 1);
+	// play turns until a player runs out of cards
 	while (currentPlayer_->getHand().size() != 0) {
 		handleTurn();
 	}
 	return;
 }
 
+// requires: valid player and command
+// modifies: plays the action, modifies the player and the Round with the action taken, may end the round
+// ensures: the next player will become the current player
 void Round::playTurn(shared_ptr<Player> player, Command command) {
 	// handle play
-	// TODO input check
 	switch (command.type_) {
 	case PLAY:
 		player->playCard(command.card_);
@@ -79,6 +100,9 @@ void Round::playTurn(shared_ptr<Player> player, Command command) {
 	currentPlayer_ = gameState_->players_.at(newPosition);
 }
 
+// requires: a list of played cards
+// ensures: nothing is modified
+// returns: list of cards that have been played this round
 vector<Card> Round::getPlayedCard() const {
 	return playedCards_;
 }
