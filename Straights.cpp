3daee3d9b65008910344704904d45d9fbeec618a
Yu Chen Hou <houyuchen66@gmail.com>
2016-06-18 23:41:25 -0400

    comment main

--- Straights.cpp
+++ Straights.cpp
@@ -6,10 +6,12 @@ using namespace std;
 
 std::shared_ptr<GameController> GameController::instance_; // WHYYY?
 
+// ensures: creates players and runs a full game of Straights
+// returns: 0 if game properly runs
 int main(int argc, char *argv[]) {
 	int seed = 0;
 	if (argc > 1) {
-		seed = atoi(argv[1]);
+		seed = atoi(argv[1]); // load seed if provided in cmd
 	}
 	shared_ptr<GameController> controller = GameController::createInstance(seed);
 
@@ -17,6 +19,7 @@ int main(int argc, char *argv[]) {
 		controller->playGame();
 	}
 	catch (Command c) {
+		// quit game if quit command has been issued
 		if (c.type_ == QUIT) {
 			return 0;
 		}
