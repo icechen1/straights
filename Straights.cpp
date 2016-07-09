047908282c7a917f47abd7bffed4d39de950d581
Yu Chen Hou <houyuchen66@gmail.com>
2016-07-09 18:55:34 -0400

    gtk setup

--- Straights.cpp
+++ Straights.cpp
@@ -1,6 +1,7 @@
 #include "GameController.h"
 #include <memory>
 #include <iostream>
+#include <gtkmm.h>
 
 using namespace std;
 
@@ -8,22 +9,33 @@ std::shared_ptr<GameController> GameController::instance_; // WHYYY?
 
 // ensures: creates players and runs a full game of Straights
 // returns: 0 if game properly runs
-int main(int argc, char *argv[]) {
-	int seed = 0;
-	if (argc > 1) {
-		seed = atoi(argv[1]); // load seed if provided in cmd
-	}
-	shared_ptr<GameController> controller = GameController::createInstance(seed);
+//int main(int argc, char *argv[]) {
+//	int seed = 0;
+//	if (argc > 1) {
+//		seed = atoi(argv[1]); // load seed if provided in cmd
+//	}
+//	shared_ptr<GameController> controller = GameController::createInstance(seed);
+//
+//	try {
+//		controller->playRound();
+//	}
+//	catch (Command c) {
+//		// quit game if quit command has been issued
+//		if (c.type_ == QUIT) {
+//			return 0;
+//		}
+//	}
+//
+//	return 0;
+//}
 
-	try {
-		controller->playRound();
-	}
-	catch (Command c) {
-		// quit game if quit command has been issued
-		if (c.type_ == QUIT) {
-			return 0;
-		}
-	}
-
-	return 0;
+int main(int argc, char *argv[])
+{
+	auto app =
+		Gtk::Application::create(argc, argv,
+			"org.gtkmm.examples.base");
+	Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("screen.glade");
+	Gtk::Window *win;
+	builder->get_widget("window1", win);
+	return app->run(*win);
 }
