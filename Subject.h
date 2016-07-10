5e1c7ac6ed6d63cbf8902de23139548676c9551b
Yu Chen Hou <houyuchen66@gmail.com>
2016-07-10 17:58:10 -0400

    good shit

--- Subject.h
+++ Subject.h
@@ -1,18 +1,18 @@
 #ifndef _SUBJECT_
 #define _SUBJECT_
-#include "View.h"
+#include "Observer.h"
 #include <memory>
 #include <vector>
 #include <algorithm>
 
 class Subject {
 public:
-	void subscribe(std::shared_ptr<View>);
-	void unsubscribe(const std::shared_ptr<View>);
+	void subscribe(std::shared_ptr<Observer>);
+	void unsubscribe(const std::shared_ptr<Observer>);
 protected:
 	void notify();
 private:
-	std::vector<std::shared_ptr<View>> observers_;
+	std::vector<std::shared_ptr<Observer>> observers_;
 };
 
 #endif
