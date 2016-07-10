0a64d6396e61df798385bcd3f2572914495afea7
Yu Chen Hou <houyuchen66@gmail.com>
2016-07-10 16:09:37 -0400

    some codes

--- Observable.cpp
+++ Observable.cpp
@@ -1,14 +1,23 @@
 #include "Observable.h"
 using namespace std;
 
-void Observable::subscribe(shared_ptr<View>)
+void Observable::subscribe(shared_ptr<View> view)
 {
+	// check if view is already a subscriber
+	if (std::find(observers_.begin(), observers_.end(), view) != observers_.end()) {
+		return;
+	}
+	observers_.push_back(view);
 }
 
-void Observable::unsubscribe(const shared_ptr<View>)
+void Observable::unsubscribe(const shared_ptr<View> view)
 {
+	observers_.erase(remove(observers_.begin(), observers_.end(), view), observers_.end());
 }
 
 void Observable::notify()
 {
+	for (shared_ptr<View> view : observers_) {
+		view->update();
+	}
 }
--- Observable.h
+++ Observable.h
@@ -2,12 +2,16 @@
 #define _OBSERVABLE_
 #include "View.h"
 #include <memory>
+#include <vector>
+#include <algorithm>
 class Observable {
 public:
 	void subscribe(std::shared_ptr<View>);
 	void unsubscribe(const std::shared_ptr<View>);
 protected:
 	void notify();
+private:
+	std::vector<std::shared_ptr<View>> observers_;
 };
 
 #endif
