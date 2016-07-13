7651eebcfe6285349efba55058a8fa2a0b86482a
Yu Chen Hou <houyuchen66@gmail.com>
2016-07-13 00:54:30 -0400

    kk

--- Straights.cpp
+++ Straights.cpp
@@ -11,10 +11,6 @@ std::shared_ptr<GameController> GameController::instance_; // WHYYY?
 // ensures: creates players and runs a full game of Straights
 // returns: 0 if game properly runs
 int main(int argc, char *argv[]) {
-	int seed = 0;
-	if (argc > 1) {
-		seed = atoi(argv[1]); // load seed if provided in cmd
-	}
 	auto app = Gtk::Application::create(argc, argv, "org.straights");
 	shared_ptr<GameView> gameView = shared_ptr<GameView>(new GameView(app));
 	//shared_ptr<GameController> controller = GameController::createInstance(seed, gameView);
